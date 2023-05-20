d10240596729f8a0e344e7302adea4d45cb370e3
kapayji2 <kapayji2@gmail.com>
2023-05-20 12:32:30 +0300

    123

---
+++ DateUtilsParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateUtils
+{
+    internal class DateUtilsParser
+    {
+        public int GetMonth(string date)
+        {
+            return DateTime.Parse(date).Month;
+        }
+    }
+}
